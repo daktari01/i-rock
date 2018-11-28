63f16f6b7cda61d5fe8f2512b744e4c55e868d51
James Dindi <dindijjames@gmail.com>
2018-11-28 10:57:24 +0300

    Test model validations

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,10 +2,22 @@ class Achievement < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :title, presence: true
+  # validates :title, uniqueness: true
+
+  validate :unique_title_for_one_user
 
   enum privacy: %i[public_access private_access friends_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
+  private
+
+  def unique_title_for_one_user
+    existing_achievement = Achievement.find_by(title: title)
+    if existing_achievement && existing_achievement.user == user
+      errors.add(:title, "You can't have the same achievement with the same title")
+    end
   end
 end
